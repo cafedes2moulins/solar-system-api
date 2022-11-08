c0c4239bcbe60c2e66995b74963ed7354a4b0888
Devin Taylor <devin.taylor122@gmail.com>
2022-11-08 12:25:51 -0800

    yay again

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
 
 
 db = SQLAlchemy()
 migrate = Migrate()
+load_dotenv()
 
 
 
@@ -12,7 +15,9 @@ def create_app(test_config=None):
     app = Flask(__name__)
 
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development"
+    app.config["SQLALCHEMY_DATABASE_URI"]=os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+
 
     from app.models.planet import Planet
 
